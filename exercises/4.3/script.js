5f9e6b4d2099909c2325b67f972842846e4e9558
Marcus C. H. Filho <mchusemann@gmail.com>
2021-03-04 23:23:19 -0300

    (fix)(identation): Fix all the identation :bug:

--- exercises/4.3/script.js
+++ exercises/4.3/script.js
@@ -1,115 +1,115 @@
-// // Exercício 1
-// console.log("Exercício 1: ");
-// console.log();
-// let numberOfAsterisks = 3;
-// let asterisk = '';
+// Exercício 1
+console.log("Exercício 1: ");
+console.log();
+let numberOfAsterisks = 3;
+let asterisk = '';
 
-// for (let indexColumn = 0; indexColumn < numberOfAsterisks; indexColumn += 1) {
-//     asterisk = '';
-//     for (let indexRow = 0; indexRow < numberOfAsterisks; indexRow += 1) {
-//         asterisk += '*';
-//     }
-//     console.log(asterisk);
-// }
+for (let indexColumn = 0; indexColumn < numberOfAsterisks; indexColumn += 1) {
+    asterisk = '';
+    for (let indexRow = 0; indexRow < numberOfAsterisks; indexRow += 1) {
+        asterisk += '*';
+    }
+    console.log(asterisk);
+}
 
-// // Exercício 2
-// console.log("==================================");
-// console.log("Exercício 2: ");
-// console.log();
-// numberOfAsterisks = 5;
-// asterisk = '';
+// Exercício 2
+console.log("==================================");
+console.log("Exercício 2: ");
+console.log();
+numberOfAsterisks = 5;
+asterisk = '';
 
-// for (let index = 0; index < numberOfAsterisks; index += 1) {
-//     asterisk += '*';
-//     console.log(asterisk);
-// }
+for (let index = 0; index < numberOfAsterisks; index += 1) {
+    asterisk += '*';
+    console.log(asterisk);
+}
 
-// // Exercício 3
+// Exercício 3
 
-// console.log("==================================");
-// console.log("Exercício 3: ");
-// console.log();
+console.log("==================================");
+console.log("Exercício 3: ");
+console.log();
 
-// numberOfAsterisks = 5;
-// asterisk = '*';
-// let line = '';
-// let position = numberOfAsterisks;
+numberOfAsterisks = 5;
+asterisk = '*';
+let line = '';
+let position = numberOfAsterisks;
 
-// for (let indexLine = 0; indexLine < numberOfAsterisks; indexLine += 1) {
-//     for (let indexColumn = 0; indexColumn <= numberOfAsterisks; indexColumn += 1) {
-//         if (indexColumn < position) {
-//             line += ' ';
-//         } else {
-//             line += asterisk;
-//         }
-//     }
-//     console.log(line);
-//     position -= 1;
-//     line = '';
-// }
+for (let indexLine = 0; indexLine < numberOfAsterisks; indexLine += 1) {
+    for (let indexColumn = 0; indexColumn <= numberOfAsterisks; indexColumn += 1) {
+        if (indexColumn < position) {
+            line += ' ';
+        } else {
+            line += asterisk;
+        }
+    }
+    console.log(line);
+    position -= 1;
+    line = '';
+}
 
-// // Exercício 4
+// Exercício 4
 
-// console.log("==================================");
-// console.log("Exercício 4: ");
-// console.log();
+console.log("==================================");
+console.log("Exercício 4: ");
+console.log();
 
-// numberOfAsterisks = 25;
-// asterisk = '*';
-// position = (numberOfAsterisks + 1) / 2;
-// let right = position;
-// let left = position;
-// line = '';
+numberOfAsterisks = 25;
+asterisk = '*';
+position = (numberOfAsterisks + 1) / 2;
+let right = position;
+let left = position;
+line = '';
 
 
-// for (let indexLine = 0; indexLine <= position; indexLine += 1) {
-//     for (let indexColumn = 0; indexColumn <= numberOfAsterisks; indexColumn += 1) {
-//         if (indexColumn > left && indexColumn < right) {
-//             line = line + asterisk;
-//         } else {
-//             line = line + ' ';
-//         }
-//     }
-//     console.log(line);
-//     line = ' ';
-//     right += 1;
-//     left -= 1;
-// }
+for (let indexLine = 0; indexLine <= position; indexLine += 1) {
+    for (let indexColumn = 0; indexColumn <= numberOfAsterisks; indexColumn += 1) {
+        if (indexColumn > left && indexColumn < right) {
+            line = line + asterisk;
+        } else {
+            line = line + ' ';
+        }
+    }
+    console.log(line);
+    line = ' ';
+    right += 1;
+    left -= 1;
+}
 
-// //Exercício 5
+//Exercício 5
 
-// console.log("==================================");
-// console.log("Exercício 5: ");
-// console.log();
+console.log("==================================");
+console.log("Exercício 5: ");
+console.log();
 
-// numberOfAsterisks = 7;
-// asterisk = '*';
-// position = (numberOfAsterisks + 1) / 2;
-// right = position;
-// left = position;
-// line = '';
+numberOfAsterisks = 7;
+asterisk = '*';
+position = (numberOfAsterisks + 1) / 2;
+right = position;
+left = position;
+line = '';
 
-// if (numberOfAsterisks % 2 === 0) {
-//     console.log("Precisa ser um número ímpar.");
-// } else {
-//     for (let indexLine = 1; indexLine <= position; indexLine += 1) {
-//         for (let indexColumn = 1; indexColumn <= numberOfAsterisks; indexColumn += 1) {
-//             if (indexColumn === left || indexColumn === right) {
-//                 line += asterisk;
-//             } else {
-//                 if(indexLine === position){
-//                  line += asterisk;   
-//                 } else {
-//                    line += ' ';
-//             }
-//             }
-//         }
-//         console.log(line);
-//         line = '';
-//         right += 1;
-//         left -= 1;
-//     }
-// }
+if (numberOfAsterisks % 2 === 0) {
+    console.log("Precisa ser um número ímpar.");
+} else {
+    for (let indexLine = 1; indexLine <= position; indexLine += 1) {
+        for (let indexColumn = 1; indexColumn <= numberOfAsterisks; indexColumn += 1) {
+            if (indexColumn === left || indexColumn === right) {
+                line += asterisk;
+            } else {
+                if (indexLine === position) {
+                    line += asterisk;
+                } else {
+                    line += ' ';
+                }
+            }
+        }
+        console.log(line);
+        line = '';
+        right += 1;
+        left -= 1;
+    }
+}
 
 // Exercício 6
 
@@ -117,16 +117,16 @@ console.log("==================================");
 console.log("Exercício 6: ");
 console.log();
 
-let primeNumber = 79;
+let primeNumber = 997;
 let divisorCount = 1;
 
-for(let index = 0; index <= primeNumber / 2; index += 1){
-    if(primeNumber % index === 0){
+for (let index = 0; index <= primeNumber / 2; index += 1) {
+    if (primeNumber % index === 0) {
         divisorCount += 1;
     }
 }
 
-if(divisorCount === 2){
+if (divisorCount === 2) {
     console.log("O número é primo.");
 } else {
     console.log("O número não é primo.");
