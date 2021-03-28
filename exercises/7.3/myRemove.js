2cf301a74b8236c391684eacbce0e28333c338ca
Marcus C. H. Filho <mchusemann@gmail.com>
2021-03-28 12:04:52 -0300

    :rocket: feat(myRemoveWithoutCopy.js): Adds the myRemoveWithoutCopy function and its tests

--- exercises/7.3/myRemove.js
+++ exercises/7.3/myRemove.js
@@ -10,6 +10,9 @@ function myRemove(arr, item) {
   return newArr;
 }
 
+const myArray = [1, 2, 3];
+
 assert.deepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 4], '[1, 2, 4]');
 assert.notDeepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 3, 4], '[1, 2, 4]');
+assert.notDeepStrictEqual(myRemove(myArray, 3), myArray, myArray);
 assert.deepStrictEqual(myRemove([1, 2, 3, 4], 5) , [1, 2, 3, 4], '[1, 2, 4]');
