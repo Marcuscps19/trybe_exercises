1d65d8d179567fcd1d9a51828cf9b105284d3073
Marcus C. H. Filho <mchusemann@gmail.com>
2021-04-07 10:55:07 -0300

    :rocket: feat(reduce): Exercises 1 to 6

---
+++ exercises/Bloco 08: Higher Order Functions do JS ES6/8.4 - reduce/exercise6.js
@@ -0,0 +1,19 @@
+const assert = require('assert');
+
+const students = ['Pedro Henrique', 'Miguel', 'Maria Clara'];
+const grades = [[9, 8, 10, 7, 5], [10, 9, 9, 10, 8], [10, 7, 10, 8, 9]];
+
+const studentAverage = () => {
+  const arrayOfGrades = grades.map((gradeArray) => gradeArray.reduce((acc, grade) => acc + grade, 0)/gradeArray.length);
+  return students.map((student, index) => {
+   return {name: student, average: arrayOfGrades[index]};
+  });
+}
+
+const expected = [
+  { name: 'Pedro Henrique', average: 7.8 },
+  { name: 'Miguel', average: 9.2 },
+  { name: 'Maria Clara', average: 8.8 },
+];
+
+assert.deepStrictEqual(studentAverage(), expected);
