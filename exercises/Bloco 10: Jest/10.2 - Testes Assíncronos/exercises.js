d921ff2cdde2cee5766a273ab9e04784b52de33a
Marcus C. H. Filho <mchusemann@gmail.com>
2021-04-23 19:33:12 -0300

    :rocket: feat: exercise 1 to 6

---
+++ exercises/Bloco 10: Jest/10.2 - Testes Assíncronos/exercises.js
@@ -0,0 +1,36 @@
+const fetch = require('node-fetch');
+
+const uppercase = (str, callback) => {
+  callback(str.toUpperCase());
+};
+
+const users = {
+  4: { name: 'Mark' },
+  5: { name: 'Paul' },
+};
+
+const findUserById = (id) => {
+  return new Promise((resolve, reject) => {
+    if (users[id]) {
+      return resolve(users[id]);
+    }
+
+    return reject({ error: 'User with ' + id + ' not found.' });
+  });
+};
+
+const getUserName = (userID) => {
+  return findUserById(userID).then((user) => user.name);
+};
+
+const getRepos = (url) => {
+  return fetch(url)
+    .then(response => response.json())
+    .then((data) => data.map((repo) => repo.name))
+};
+
+module.exports = {
+  uppercase,
+  getUserName,
+  getRepos,
+};
---
+++ exercises/Bloco 10: Jest/10.2 - Testes Assíncronos/exercises.test.js
@@ -0,0 +1,57 @@
+const { uppercase, getUserName, getRepos } = require('./exercises');
+
+describe('Exercise 1', () => {
+    
+    it('uppercase exercise1 to EXERCISE1', (done) => {
+        uppercase('exercise1', (str) => {
+            expect(str).toBe('EXERCISE1');
+            done();
+        });
+    });
+});
+
+describe('Exercise 2', () => {
+
+    it('exercise 2.1', async () => await expect(getUserName(4)).resolves.toEqual('Mark'));
+
+    it('exercise 2.2', async () => {
+        expect.assertions(1);
+        try {
+            await getUserName(2);
+        } catch(error) {
+            expect(error).toEqual({error: 'User with 2 not found.'});
+        }
+    });
+});
+
+describe('Exercise 4', () => {
+    test('Exercise 4.1', () => {
+        return getRepos('https://api.github.com/orgs/tryber/repos').then(data => {
+            expect(data).toContain('sd-01-week4-5-project-todo-list');
+            expect(data).toContain('sd-01-week4-5-project-meme-generator');
+        });
+    });
+});
+
+// 1 - beforeEach
+// 1 - test 
+// 1 - afterEach
+// 1 -  beforeEach
+// 2 - beforeEach
+// 2 - test
+// 1 - afterEach
+// 2 - afterEach
+
+// beforeEach(() => console.log('1 - beforeEach'));
+// afterEach(() => console.log('1 - afterEach'));
+
+// test('', () => console.log('1 - test'));
+
+// describe('Scoped / Nested block', () => {
+//   beforeEach(() => console.log('2 - beforeEach'));
+//   afterEach(() => console.log('2 - afterEach'));
+
+//   test('', () => console.log('2 - test'));
+// });
+
+
